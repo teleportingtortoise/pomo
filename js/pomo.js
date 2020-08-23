66c5ee0259b9ddf25cea6ca836652e22d357d2b3
teleportingtortoise <39284283+teleportingtortoise@users.noreply.github.com>
2020-08-23 01:52:20 -0700

    minor changes and seperate css from html

--- js/pomo.js
+++ js/pomo.js
@@ -10,17 +10,18 @@ window.onload = function() {
 
     function restart() {
         if (timer.elapsedS >= timer.duration) {
-            setTimeout(function() { timer.run(); }, 1000);
+            setTimeout(function() { timer.duration = timeAmountS; timer.run(); }, 1000);
         }
     }
 
     function format() {
         counter.innerText = ('00' + ((timer.remainingS / 60) | 0)).slice(-2) + ':' + ('00' + (timer.remainingS % 60)).slice(-2);
-        display.style.width = Math.map(timer.elapsedM,0,timeAmountM,0,100) + '%'
-        display.style.backgroundColor = 'rgb(' + Math.map(timer.elapsedM,0,timeAmountM,96,216) + ',' + Math.map(timer.elapsedM,0,timeAmountM,216,64) + ',' + '032)'
+        display.style.width = Math.map(timer.elapsedM,0,timer.duration * 1000,0,100) + '%'
+        display.style.backgroundColor = 'rgb(' + Math.map(timer.elapsedM,0,timer.duration * 1000,96,216) + ',' + Math.map(timer.elapsedM,0,timer.duration * 1000,216,64) + ',' + '032)'
     }
 
-    function setTime() {
-        
+    this.setTime = function() {
+        var variable = document.getElementById("input_id").value;
+        timeAmountS = variable;
     }
 }
